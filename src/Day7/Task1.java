c05624b3d614eb598393630cba008716fb0bfd98
Alexander Leros <alexander.leros@gmail.com>
2023-03-17 17:27:35 +0300

    added day 6

---
+++ src/Day7/Task1.java
@@ -0,0 +1,14 @@
+package Day7;
+
+
+public class Task1 {
+    public static void main(String[] args) {
+        Airplane airplane1 = new Airplane(" Boing", 2010, 40, 40000 );
+        Airplane airplane2 = new Airplane(" Boing", 2010, 40, 40000 );
+
+        Airplane.compareAirplanes(airplane1, airplane2);
+
+
+
+    }
+}
---
+++ src/Day7/Task2.java
@@ -0,0 +1,29 @@
+package Day7;
+
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        Player player1 = new Player(78);
+        Player player2 = new Player(99);
+        Player player3 = new Player(65);
+
+        Player.info();
+
+        Player player4 = new Player(100);
+        Player player5 = new Player(76);
+        Player player6 = new Player(89);
+
+        Player.info();
+
+        Player player7 = new Player(88);
+
+        Player.info();
+
+        for (int i = 0; i < 65; i++);
+        player3.run();
+
+        Player.info();
+    }
+
+}
